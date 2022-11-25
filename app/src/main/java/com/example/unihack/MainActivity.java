ec7008ee5df4863f36819a2a5f2a4a0e30ee370e
lupsavlad <lupsavlad51@gmail.com>
2022-11-25 23:38:24 +0200

    main activity 10%

--- app/src/main/java/com/example/unihack/MainActivity.java
+++ app/src/main/java/com/example/unihack/MainActivity.java
@@ -9,12 +9,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1=findViewById(R.id.text1);
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         
 
     }
