de4bf52ac4b2ea4d7f6307db60bc25297603cfdd
lupsavlad <lupsavlad51@gmail.com>
2022-11-26 12:04:48 +0200

    incercare de streetview

--- app/src/main/java/com/example/unihack2022/MapActivity.java
+++ app/src/main/java/com/example/unihack2022/MapActivity.java
@@ -12,6 +12,11 @@ import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 
 
 import android.Manifest;
@@ -36,6 +41,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -44,9 +50,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.SupportMapFragment;
 
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback  {
 
     CardView cardView;
+
+    static final LatLng HAMBURG = new LatLng(45.76138917716781, 21.232814374031086);
 
     private GoogleMap mMap;
 
@@ -62,6 +70,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         cardView = findViewById(R.id.cardView);
 
 
+
+
+
+
         // Get the SupportMapFragment and request notification when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -71,11 +83,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng faleza = new LatLng(44.20038727708174, 28.655086442904665);
-        mMap.addMarker(new MarkerOptions()
-                .position(faleza)
-                .title("Parcare Faleza")
-                .snippet("Locuri libere:8  ||  Tarif:2€/ora"));
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(@NonNull Marker marker) {
@@ -86,8 +93,17 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     cardView.setVisibility(View.VISIBLE);
                     pressed = 1;
                 }
+                return false;
             }
         });
+        mMap.addMarker(new MarkerOptions()
+                .position(HAMBURG)
+        );
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(HAMBURG));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG,13));
     }
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+}
 
 
---
+++ app/src/main/java/com/example/unihack2022/StreetViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.unihack2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StreetViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
