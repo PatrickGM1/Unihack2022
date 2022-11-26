32da135c54181e4fcb132f8d83d978bc8b5754c8
lupsavlad <lupsavlad51@gmail.com>
2022-11-26 03:26:06 +0200

    ceva progres? posibil

--- app/src/main/java/com/example/unihack2022/MainActivity.java
+++ app/src/main/java/com/example/unihack2022/MainActivity.java
@@ -10,14 +10,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MapActivity.class);
