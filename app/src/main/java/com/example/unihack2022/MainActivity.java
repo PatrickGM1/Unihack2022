6c9ddd48f8fb8db35aa5d4f837967ecce62ab096
PatrickGM1 <patrickgheba@gmail.com>
2022-11-26 03:38:19 +0200

    Added login/register

--- app/src/main/java/com/example/unihack2022/MainActivity.java
+++ app/src/main/java/com/example/unihack2022/MainActivity.java
@@ -2,14 +2,34 @@ package com.example.unihack2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button, button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
     }
 }
